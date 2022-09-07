b87a11afdfbed20fa258cfbccd77b44a8eac43a9
o_chapel <8799@ukr.net>
2022-09-07 13:37:26 +0300

    Create HomeFragment, create navigation, implement view binding, set up YouTubeApi key

--- app/src/main/java/com/och/musicplayer/ui/MainActivity.kt
+++ app/src/main/java/com/och/musicplayer/ui/MainActivity.kt
@@ -1,11 +1,17 @@
-package com.och.musicplayer
+package com.och.musicplayer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.och.musicplayer.R
+import com.och.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/och/musicplayer/ui/home/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.och.musicplayer.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.och.musicplayer.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var binding: FragmentHomeBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
+    }
+}
